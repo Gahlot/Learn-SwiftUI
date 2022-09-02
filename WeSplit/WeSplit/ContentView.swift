ca0aad76ccc00a424a9bf854eb37c1c09c27869d
Mahesh Gahlot <mahesh.gahlot@finoit.co.in>
2022-09-02 22:47:50 +0530

    State Property wrapper

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -4,28 +4,17 @@
 //
 //  Created by macadmin on 02/09/22.
 //
-
+//views are a function of their state
+/*
+ When we say SwiftUI’s views are a function of their state, we mean that the way your user interface looks – the things people can see and what they can interact with – are determined by the state of your program. For example, they can’t tap Continue until they have entered their name in a text field.
+ */
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
     var body: some View {
-        NavigationView{
-            Form {
-                Section{
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                Section{
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                
-            }
-            .navigationTitle("Swift Form")        // large title by default
-             //.navigationBarTitleDisplayMode(.inline)  // for small title
+        Button("tap Count: \(tapCount)") {
+            tapCount += 1
         }
         
     }
