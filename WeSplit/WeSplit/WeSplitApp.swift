cf380286b5d642af2e726e2cac7f124204495a40
Mahesh Gahlot <mahesh.gahlot@finoit.co.in>
2022-09-02 22:16:20 +0530

    Day 1 Basic of SwiftUI and Form View

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by macadmin on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
