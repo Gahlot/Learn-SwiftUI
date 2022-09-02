9f4423e85db1b117ef44aad7b147b2229d4fb99b
Mahesh Gahlot <mahesh.gahlot@finoit.co.in>
2022-09-02 23:05:32 +0530

    Binding State to user interface

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -5,16 +5,15 @@
 //  Created by macadmin on 02/09/22.
 //
 //views are a function of their state
-/*
- When we say SwiftUI’s views are a function of their state, we mean that the way your user interface looks – the things people can see and what they can interact with – are determined by the state of your program. For example, they can’t tap Continue until they have entered their name in a text field.
- */
+
 import SwiftUI
 
 struct ContentView: View {
-    @State var tapCount = 0
+    @State private var name = ""
     var body: some View {
-        Button("tap Count: \(tapCount)") {
-            tapCount += 1
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("your Name:- \(name)")
         }
         
     }
