44569a7caf1ea0c89f7fea7f130c014745b05ee6
Mahesh Gahlot <mahesh.gahlot@finoit.co.in>
2022-09-03 11:43:14 +0530

    Picker in form

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -14,15 +14,24 @@ struct ContentView: View {
     @State private var tipPercentage = 10
     var tipPercentages = [10, 20, 30, 0]
     var body: some View {
-        Form {
-            Section {
-                TextField("", value: $amountValue, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                    .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    
+                    TextField("", value: $amountValue, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
+                Section {
+                    Picker.init("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
             }
-            Section {
-                Text(amountValue, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-            }
+            .navigationTitle("WeSplit")
         }
+        
     }
 }
 
